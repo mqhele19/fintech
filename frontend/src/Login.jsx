8e3c1009e82bfc4449b4683cd091bdf9db7fa8d1
mqhele mguni <mqhelemguni19@gmail.com>
2023-08-08 16:20:36 +0200

    second commit

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
+import axios from 'axios'
+
 
 function Login() {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('', values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+
+    }
+
+
     return(
-        <div className='d-flex justify-content-center align-items-center vh-100'>
-            <div className='bg-white p-3 rounded w-25'>
+        <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+            <div className='bg-white p-3 rounded w-25 border'>
             <h2>Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor="email"><strong>Email</strong></label>
                     <input type="email" placeholder='Enter Email' name='email'
+                    onChange={e => setValues({...values, email: e.target.value})}
                     className='form-control rounded-0' />
                     
                 </div>
                 <div className='mb-3'>
                     <label htmlFor="password"><strong>Password</strong></label>
                     <input type="password" placeholder='Enter Password' name='password'
+                    onChange={e => setValues({...values, password: e.target.value})}
                     className='form-control rounded-0'/>
                 </div>
                 <button type='submit' className='btn btn-success w-100 rounded-0' > Log in</button>
